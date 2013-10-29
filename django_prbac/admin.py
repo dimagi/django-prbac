05adeebbb86ed4d7a1d10d7587b26bc913a5b86d
Kenn Knowles <kknowles@dimagi.com>
2013-10-29 16:01:49 -0400

    Add admin site registration

---
+++ django_prbac/admin.py
@@ -0,0 +1,7 @@
+
+from django.contrib import admin
+
+from django_prbac.models import *
+
+admin.site.register(Role)
+admin.site.register(Grant)
--- django_prbac/mock_settings.py
+++ django_prbac/mock_settings.py
@@ -1,5 +1,11 @@
+"""
+Settings just for running the django-prbac tests or checking out
+the admin site.
+"""
 
 SECRET_KEY='Not a secret key at all, actually'
+
+DEBUG=True
 
 DATABASES = {
     'default': {
@@ -9,6 +15,20 @@ DATABASES = {
 }
 
 INSTALLED_APPS = [
-    'django_prbac',
+    # Django apps necessary to run the admin site
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+
+    # And south
     'south',
+
+    # And this app
+    'django_prbac',
 ]
+
+STATIC_URL='/static/'
+
+ROOT_URLCONF='django_prbac.urls'
