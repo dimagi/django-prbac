0b3d54fa421c37cae74563c04a52340e065a574c
Biyeun Buczyk <biyeun@dimagi.com>
2014-02-13 21:18:18 -0600

    modify require_privilege to also check the user’s privileges and raise Http404 when no matches can be found

--- django_prbac/decorators.py
+++ django_prbac/decorators.py
@@ -2,33 +2,42 @@
 from __future__ import unicode_literals, absolute_import, print_function
 
 # Local Imports
-from django_prbac.exceptions import PermissionDenied
-from django_prbac.models import Role
+from django.http import Http404
+from django_prbac.models import Role, UserRole
+
 
 def requires_privilege(slug, **assignment):
     def decorate(fn):
         """
         Returns a function equivalent to `fn` but that requires
         a role with slug `slug` to be reachable from `request.role`
+        or `request.user.prbac_role`
         with the parameters specified in `assignment`
-        (in a parameterizes fashion)
+        (in a parameterized fashion)
         """
         def wrapped(request, *args, **kwargs):
 
             if not hasattr(request, 'role'):
-                raise PermissionDenied()
+                raise Http404()
 
             roles = Role.objects.filter(slug=slug)
             if not roles:
-                raise PermissionDenied()
+                raise Http404()
 
             privilege = roles[0].instantiate(assignment)
-            if not request.role.has_privilege(privilege):
-                raise PermissionDenied()
+            if request.role.has_privilege(privilege):
+                return fn(request, *args, **kwargs)
+
+            try:
+                request.user.prbac_role
+            except UserRole.DoesNotExist:
+                raise Http404()
+
+            if not request.user.prbac_role.has_privilege(privilege):
+                raise Http404()
 
             return fn(request, *args, **kwargs)
 
         return wrapped
 
     return decorate
-
