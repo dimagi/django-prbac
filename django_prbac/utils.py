a7cd9494e3cec9cb669f33c32a085d0022221e2d
Ethan Soergel <esoergel@gmail.com>
2015-05-05 09:11:43 +0000

    Log to console time spent calculating permissions

--- django_prbac/utils.py
+++ django_prbac/utils.py
@@ -11,27 +11,30 @@ def ensure_request_has_privilege(request, slug, **assignment):
     Ensures that an HttpRequest object has the privilege specified by slug.
     If it does not, it throws a PermissionDenied Exception.
     """
-    if not hasattr(request, 'role'):
-        raise PermissionDenied()
+    from timeit import timeit
+    def do():
+        if not hasattr(request, 'role'):
+            raise PermissionDenied()
 
-    roles = Role.objects.filter(slug=slug)
-    if not roles:
-        raise PermissionDenied()
+        roles = Role.objects.filter(slug=slug)
+        if not roles:
+            raise PermissionDenied()
 
-    privilege = roles[0].instantiate(assignment)
-    if request.role.has_privilege(privilege):
-        return
+        privilege = roles[0].instantiate(assignment)
+        if request.role.has_privilege(privilege):
+            return
 
-    if not hasattr(request, 'user') or not hasattr(request.user, 'prbac_role'):
-        raise PermissionDenied()
+        if not hasattr(request, 'user') or not hasattr(request.user, 'prbac_role'):
+            raise PermissionDenied()
 
-    try:
-        request.user.prbac_role
-    except UserRole.DoesNotExist:
-        raise PermissionDenied()
+        try:
+            request.user.prbac_role
+        except UserRole.DoesNotExist:
+            raise PermissionDenied()
 
-    if not request.user.prbac_role.has_privilege(privilege):
-        raise PermissionDenied()
+        if not request.user.prbac_role.has_privilege(privilege):
+            raise PermissionDenied()
+    print("{} seconds calculating privileges".format(timeit(do, number=1)))
 
 
 def has_privilege(request, slug, **assignment):
