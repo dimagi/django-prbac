ac7c63304aa6ee4390096dcc24b2df5c7593cf27
Daniel Miller <dmiller@dimagi.com>
2017-10-27 12:32:00 -0400

    Get package and docs version from __init__.py
    The package version is now in one place only.

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import os
+import re
 from setuptools import setup, find_packages
+
+def get_version():
+    version_re = re.compile(r"""^__version__ = (['"])(.*?)\1$""", re.M)
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(thisdir, "django_prbac/__init__.py")
+    with open(path) as fh:
+        return version_re.search(fh.read()).group(2)
 
 setup(
     name='django-prbac',
-    version='0.0.3',
+    version=get_version(),
     description='Parameterized Role-Based Access Control for Django',
     author='Dimagi',
     author_email='dev@dimagi.com',
