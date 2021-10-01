b4b1b22ab9aea6ff9ea27fbb2079d587fa312f82
Ben Harling <bharling@crowdcomms.co.uk>
2021-10-01 08:44:24 +0100

    fix(tests): user get_user_model instead of referring directly to auth.models.User

--- django_prbac/arbitrary.py
+++ django_prbac/arbitrary.py
@@ -1,7 +1,9 @@
 import uuid
 from random import choice
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 from django_prbac.models import *
 
