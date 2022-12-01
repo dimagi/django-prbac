8a65ba82f7a54297349c8f9108b7d41b34d6706e
Daniel Miller <dmiller@dimagi.com>
2022-12-01 12:28:39 -0500

    Drop support for EOL Python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
