1eb9aa0a233266c4b7a24682b445ef7c9e0754b2
Daniel Miller <dmiller@dimagi.com>
2022-12-01 12:33:19 -0500

    Add Python 3.9 to the build matrix
    Apparently 3.10 and 3.11 are not (yet?) available on Travis?
    
    $ curl -sSf --retry 5 -o python-3.10.tar.bz2 ${archive_url}
    curl: (22) The requested URL returned error: 404 Not Found
    Unable to download 3.10 archive. The archive may not exist. Please consider a different version.
    
    $ curl -sSf --retry 5 -o python-3.11.tar.bz2 ${archive_url}
    curl: (22) The requested URL returned error: 404 Not Found
    Unable to download 3.11 archive. The archive may not exist. Please consider a different version.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     options={"bdist_wheel": {"universal": "1"}},
