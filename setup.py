a4537ab51890c9860e300f1276fe54c0894b8c0d
Graham Herceg <gherceg@dimagi.com>
2023-11-14 14:42:50 -0500

    update setup.py to reflect current state
    - support python 3.8 - 3.11
    - support django 3 and 4, drop support for 2

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        # avoid django 2 <2.2.10 and django 3 < 3.0.7
+        # avoid django 3 < 3.0.7
         # https://github.com/advisories/GHSA-hmr4-m2h5-33qx
-        'django>=2.2.13,!=3.0.0,!=3.0.1,!=3.0.2,!=3.0.3,!=3.0.4,!=3.0.5,!=3.0.6,<4',
+        'django>=3.0.7,<5',
         'jsonfield>=1.0.3,<4',
         'simplejson',
     ],
@@ -45,9 +45,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     options={"bdist_wheel": {"universal": "1"}},
